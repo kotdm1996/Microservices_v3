657aa6eafb4d62b84d4b79c98cf25723f9172dd0
kotdm1996 <dima.kotlarsky@gmail.com>
2025-03-12 19:14:50 -0400

    roll back
    roll back

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://aca57ec68c3d54195a1eb372c021a6ec-213032237.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
