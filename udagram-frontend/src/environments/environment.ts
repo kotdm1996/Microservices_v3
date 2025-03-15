b08975fc874411f4531a402b2d1e42cacda8e4c1
kotdm1996 <dima.kotlarsky@gmail.com>
2025-03-14 23:21:52 -0400

    20250314 test
    20250314 test

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a8a039ad36f454c208add116bbb112d3-562510163.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://ac96dc3c575b34c7b9db13addb42c69d-280321362.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
